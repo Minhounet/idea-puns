eb499868e8a963a18724241b6cf4e82ade9d780e
Quang-Minh TRAN <27333782+Minhounet@users.noreply.github.com>
2025-10-19 13:54:31 +0200

    Create build.gradle.kts for infra

---
+++ infra/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+}
+
+group = "com.minhounet.ideapuns.infra"
+version = "1.0-SNAPSHOT"
+
+dependencies {
+    // Add infra-specific dependencies here
+}
